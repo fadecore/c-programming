62f1470be62b12758132f55a57e49c3740517622
fade <ttest@test.com>
2018-09-05 22:07:33 +0200

    Zeichenkonstanten

---
+++ 4.Beipiel-Zeichenkonstanten.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main(void)
+{
+  char z0 = 72;   // "H"
+  char z1 = 'a';  // 97
+  char z2 = 108;  // "l"
+  char z3 = 'l';  // 108
+  char z4 = 111;  // 'o'
+
+  printf("Ausgabe: %c %c %c %c %c", z0, z1, z2, z3, z4);
+  // Ausgabe: H A L L O
+}
